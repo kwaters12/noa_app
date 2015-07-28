6df9e55afa083bc459078fb2f94d33fbaea3729b
kwaters12 <kellywaters@gmail.com>
2015-07-27 21:15:46 -0700

    add favicon

--- app/models/dropbox_file.rb
+++ app/models/dropbox_file.rb
@@ -1,11 +1,11 @@
-class DropboxFile < ActiveRecord::Base
+# class DropboxFile < ActiveRecord::Base
 
-  attr_accessible :url , :created_at ,:updated_at, :document
+#   attr_accessible :url , :created_at ,:updated_at, :document
  
-   has_attached_file :document ,
+#    has_attached_file :document ,
  
-    :url => "/system/:class/:attachment/:id/:style/:basename.:extension",
-    :path => "#{Rails.root}/public/system/:class/:attachment/:id/:style/:basename.:extension",
-    :storage => ((Rails.env.production? || Rails.env.staging?) ? :s3 : :filesystem)
+#     :url => "/system/:class/:attachment/:id/:style/:basename.:extension",
+#     :path => "#{Rails.root}/public/system/:class/:attachment/:id/:style/:basename.:extension",
+#     :storage => ((Rails.env.production? || Rails.env.staging?) ? :s3 : :filesystem)
   
-end
+# end
